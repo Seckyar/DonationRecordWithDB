0c30f050a700818266994efe4e557cb5131bc329
Seckyar <xero.mz93@gmail.com>
2025-10-22 11:20:29 +0630

    testing for deployment

--- server.js
+++ server.js
@@ -27,7 +27,6 @@ app.use(session({
   saveUninitialized: false
 }));
 
-// === Middleware for authentication ===
 function requireLogin(req, res, next) {
   if (!req.session.user) {
     return res.status(401).json({ success: false, message: 'Unauthorized. Please log in.' });
